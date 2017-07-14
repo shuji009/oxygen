a623b698f8a41c97b18f0a961db172f49991e327
Shuji009 <shuji@music.email.ne.jp>
2017-07-14 10:43:31 +0900

    Add files via upload

---
+++ oxygensample.ino
@@ -0,0 +1,13 @@
+// 酸素センサーのサンプル
+// センサーは補聴器用電池PR44を使用
+// arduino-UDEの tool -> シリアルプロット　で利用可能
+//
+void setup() {
+	Serial.begin(115200);
+}
+void loop() {
+	int v = analogRead(A0); // A0に電池の＋　抵抗入れてね。
+	Serial.println(v);
+	delay(50); //　試験なので50ms毎にしている
+}
+
